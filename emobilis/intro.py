8303ed9298a41ad0dce1710fa4cc6c9fba1b9506
= <=>
2023-09-18 03:24:25 +0300

    Emobilis introduction to python

---
+++ emobilis/intro.py
@@ -0,0 +1,23 @@
+print(23.5)
+name = "Brian"
+
+age = 21
+
+price = 100.50
+
+pi = 22/7
+
+MAX_WITHDRAWAL_LIMIT = 100000
+
+PI = 3.142
+
+is_raining = True
+is_rich = False
+
+print(name)
+
+print(f"{name} is {age} years old")
+
+
+
+
---
+++ emobilis/numbers.py
@@ -0,0 +1,14 @@
+num1 = 1000
+
+num2 = 54
+
+print(num1 - num2)
+print(num1 * num2)
+print(num1 + num2)
+print(num1 ** 2)
+print(num1 / num2)
+print(num1 // num2)
+print(num1 % num2)
+
+print(4 + 5 /2.5 * 4 + 45 / 5)
+
