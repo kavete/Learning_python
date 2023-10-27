88f1cec185d5fe1081e828eb65afba032ed88de9
= <=>
2023-10-27 13:01:50 +0300

    Revised the code and reformatted  code. Migrated to PyCharm

---
+++ emobilis/py_variables.py
@@ -0,0 +1,31 @@
+x = 12
+y = 56
+pi = 22 / 7
+
+radius = 35.5
+
+name = "Brian"
+
+sentence = "The quick brown fox jumps over the lazy dog"
+
+distance_to_moyale = 750
+
+person_no_1 = "Doe"  # String
+population = "10000"  # String
+
+age = 21  # int
+
+price = 100.50  # float
+
+MAX_WITHDRAWAL_LIMIT = 100000
+
+PI = 3.142
+
+is_raining = True
+is_rich = False
+
+print(pi)
+
+print(name, price, is_raining)
+
+print(f"{name} is {age} years old")
